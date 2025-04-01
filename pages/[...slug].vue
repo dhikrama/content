4bd241d1aeb8573c4a22b0c7260a9916bf4ef7ed
me <dhikrama.creator@gmail.com>
2025-04-01 11:36:57 +0700

    deploy

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,16 +1,19 @@
-<script setup lang="ts">
-
+<script lang="ts" setup>
 const route = useRoute()
 const { data: page } = await useAsyncData(route.path, () => {
   return queryCollection('content').path(route.path).first()
 })
-useSeoMeta({
-  title: page.value?.title,
-  description: page.value?.description
-})
 </script>
 
 <template>
-  <ContentRenderer v-if="page" :value="page" />
-  <div v-else>Home not found</div>
+  <template v-if="page">
+    <ContentRenderer :value="page" />
+  </template>
+  <template v-else>
+    <div class="empty-page">
+      <h1>Page Not Found</h1>
+      <p>Oops! The content you're looking for doesn't exist.</p>
+      <NuxtLink to="/">Go back home</NuxtLink>
+    </div>
+  </template>
 </template>
