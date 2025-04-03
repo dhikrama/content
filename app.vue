e2f209c4e2175f7407da7bfa74e1170a20ebbfad
me <dhikrama.creator@gmail.com>
2025-04-03 20:02:26 +0700

    first commit

--- app.vue
+++ app.vue
@@ -3,14 +3,8 @@
 <template>
   <UApp>
     <LayoutsHeader />
-    <Suspense>
-      <template #default>
         <NuxtPage />
       </template>
-      <template #fallback>
-        <div class="text-center">Loading...</div>
-      </template>
-    </Suspense>
     <LayoutsFooter />
   </UApp>
 </template>
